e264735f52873c28b8eb7328cfb7376dd04116be
Dominic Brown <yeahdom@gmail.com>
2017-03-24 09:53:15 -0700

    add basic html layout, minimal styles, script boilerplate

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  alert("page loaded");
+
+
+  $("#mainform_drops").submit(function(event) {
+    event.preventDefault();
+    //grab question answers
+    var question1 = $("#question1_drop :selected").val();
+    var question1 = $("#question2_drop :selected").val();
+    var question1 = $("#question3_drop :selected").val();
+    var question1 = $("#question4_drop :selected").val();
+    var question1 = $("#question5_drop :selected").val();
+
+    $("#out1").text(question1);
+    $("#out2").text(question2);
+    $("#out3").text(question2);
+    $("#out4").text(question2);
+    $("#out5").text(question2);
+
+    $(".question_output").show();
+
+    $("#reset_btn").click(function() {
+    location.reload();
+  });
+});
