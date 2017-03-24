5e6069da6761e52c4613d9572566cda157fdcb2f
Dominic Brown <yeahdom@gmail.com>
2017-03-24 11:26:36 -0700

    fix form's jQuery fucntion

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 
   $("#mainform_drops").submit(function(event) {
-    alert("button function");
+    event.preventDefault();
 
     var question1 = $("#question1_drop :selected").val();
-    var question1 = $("#question2_drop :selected").val();
-    var question1 = $("#question3_drop :selected").val();
-    var question1 = $("#question4_drop :selected").val();
-    var question1 = $("#question5_drop :selected").val();
+    var question2 = $("#question2_drop :selected").val();
+    var question3 = $("#question3_drop :selected").val();
+    var question4 = $("#question4_drop :selected").val();
+    var question5 = $("#question5_drop :selected").val();
 
     $("#out1").text(question1);
     $("#out2").text(question2);
-    $("#out3").text(question2);
-    $("#out4").text(question2);
-    $("#out5").text(question2);
+    $("#out3").text(question3);
+    $("#out4").text(question4);
+    $("#out5").text(question5);
 
     // $(".question_output").show();
 
-    $("#reset_btn").click(function() {
-    location.reload();
+    $("#reload_btn").click(function() {
+      location.reload();
+      alert("reset pushed");
     });
-    event.preventDefault();
 
   });
 
