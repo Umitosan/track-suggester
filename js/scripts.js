26809123c8a4bec0509bbe838fa6c8c9c7282734
Dominic Brown <yeahdom@gmail.com>
2017-03-24 11:20:09 -0700

    fix logo, finish topbar links

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 
-  alert("page loaded");
-
-
   $("#mainform_drops").submit(function(event) {
-    event.preventDefault();
-    //grab question answers
+    alert("button function");
+
     var question1 = $("#question1_drop :selected").val();
     var question1 = $("#question2_drop :selected").val();
     var question1 = $("#question3_drop :selected").val();
@@ -18,9 +15,13 @@ $(document).ready(function() {
     $("#out4").text(question2);
     $("#out5").text(question2);
 
-    $(".question_output").show();
+    // $(".question_output").show();
 
     $("#reset_btn").click(function() {
     location.reload();
+    });
+    event.preventDefault();
+
   });
+
 });
