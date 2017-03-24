cb77bb599f94d6c40081e8991342c2ec12e441c4
Dominic Brown <yeahdom@gmail.com>
2017-03-24 16:23:41 -0700

    add question 4-5 logic boilerplate

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
     $(".question_output_tmp").show();
     $("#answer_title").show();
 
+    //QUESTION 1 LOGIC
     if ((question1 === "Please select") && (question2 === "Please select") && (question3 === "Please select") && (question4 === "Please select") && (question5 === "Please select")) {
       alert("You didn't select anything, why not give it a try?");
       location.reload();
@@ -32,11 +33,58 @@ $(document).ready(function() {
       alert("Question 1 is empty");
     }
 
-    // "ruby_rails"
-    // "php_drupal"
-    // "java_android"
-    // "css_design"
-    // "c#_net"
+    //QUESTION 2 LOGIC
+    if ((question1 === "Please select") && (question2 === "Please select") && (question3 === "Please select") && (question4 === "Please select") && (question5 === "Please select")) {
+      alert("You didn't select anything, why not give it a try?");
+      location.reload();
+    } else if ((question2 === "") || (question2 === "")) {
+
+    } else if ( (question2 === "") ) {
+
+    } else {
+      alert("Question 2 is empty");
+    }
+
+    //QUESTION 3 LOGIC
+    if ((question1 === "Please select") && (question2 === "Please select") && (question3 === "Please select") && (question4 === "Please select") && (question5 === "Please select")) {
+      alert("You didn't select anything, why not give it a try?");
+      location.reload();
+    } else if ((question3 === "") || (question3 === "")) {
+
+    } else if ( (question3 === "") ) {
+
+    } else {
+      alert("Question 3 is empty");
+    }
+
+    //QUESTION 4 LOGIC
+    if ((question1 === "Please select") && (question2 === "Please select") && (question3 === "Please select") && (question4 === "Please select") && (question5 === "Please select")) {
+      alert("You didn't select anything, why not give it a try?");
+      location.reload();
+    } else if ((question4 === "") || (question4 === "")) {
+
+    } else if ( (question4 === "") ) {
+
+    } else {
+      alert("Question 4 is empty");
+    }
+    //QUESTION 5 LOGIC
+    if ((question1 === "Please select") && (question2 === "Please select") && (question3 === "Please select") && (question4 === "Please select") && (question5 === "Please select")) {
+      alert("You didn't select anything, why not give it a try?");
+      location.reload();
+    } else if ((question5 === "") || (question5 === "")) {
+
+    } else if ( (question5 === "") ) {
+
+    } else {
+      alert("Question 5 is empty");
+    }
+
+    // ".ruby_rails"
+    // ".php_drupal"
+    // ".java_android"
+    // ".css_design"
+    // ".c#_net"
 
     $("#show_all").click(function() {
       $(".ruby_rails").show();
